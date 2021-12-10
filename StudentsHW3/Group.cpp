918ecea2c281d6ef91735059652f4d9e128ae8dd
Gevork Sofoyan <aab8117@gmail.com>
2021-12-10 13:45:33 +0300

    13:45

--- StudentsHW3/Group.cpp
+++ StudentsHW3/Group.cpp
@@ -1,1 +1,2 @@
 #include "Group.h"
+
--- StudentsHW3/StudentsHW3.cpp
+++ StudentsHW3/StudentsHW3.cpp
@@ -12,11 +12,15 @@ int main()
 	// ...
 	//Group gr("<БВ-111>", Group::ReadFile(path1), Group::ReadFile(path2), Group::ReadFile(path3));
 
-	Group gr("<БВ-111>", Group::ReadFile("Student.txt"), Group::ReadFile("Subject.txt"), Group::ReadFile("score table.txt"));
-	//gr.scoreTable();
-	//gr.avgScoreStud();
-	//gr.avgScoreSubj();
-	//gr.avgScoreGroup();
+	Group gr("<ОД-11>", Group::ReadFile("Student.txt"), Group::ReadFile("Subject.txt"), Group::ReadFile("score table.txt"));
+	gr.scoreTable();
+	cout << endl << endl;
+	gr.avgScoreStud();
+	cout << endl << endl;
+	gr.avgScoreSubj();
+	cout << endl << endl;
+	gr.avgScoreGroup();
+	cout << endl<<endl;
 	gr.scoreTableMinMax();
 }
 
